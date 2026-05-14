5b1dcaea54097edff78cfd8a7e5cad7889a3dde6
Nishan Subedi <nishansubedi665@gmail.com>
2026-05-14 19:56:42 +0545

    kfanishantutorial1 code

---
+++ src/variable.java
@@ -0,0 +1,18 @@
+public class variable {
+    public static void main(String[] args) {
+
+
+        int age = 20;
+        double salary = 45678.90;
+        boolean isStudent = true;
+        char grade = 'A';
+        String name = "John";
+
+
+        System.out.println("Integer value: " + age);
+        System.out.println("Double value: " + salary);
+        System.out.println("Boolean value: " + isStudent);
+        System.out.println("Character value: " + grade);
+        System.out.println("String value: " + name);
+    }
+}
